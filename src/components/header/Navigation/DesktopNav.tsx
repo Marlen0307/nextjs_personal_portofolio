124cee12cb553620f34af8f64bf653fb20b5ae7e
Marlen Hima <marlen.hima@gmail.com>
2022-08-26 14:21:35 +0200

    add config for pages and show them on desktop navbar

--- src/components/header/Navigation/DesktopNav.tsx
+++ src/components/header/Navigation/DesktopNav.tsx
@@ -1,11 +1,34 @@
-import { Flex, Show } from "@chakra-ui/react";
-import React from "react";
+import { Box, Flex, Show } from "@chakra-ui/react";
+import Link from "next/link";
+import React, { Children } from "react";
+import { pages, SitePages } from "../../../config/pages";
+import NavigationItem from "../../utils/Navigation/NavigationItem";
 import Logo from "../Logo";
 
 export default function DesktopNav() {
   return (
     <Show above="lg">
-      <Logo />
+      <Box>
+        <Logo />
+      </Box>
+      <Box
+        alignItems={"center"}
+        flexGrow={0.5}
+        display={"flex"}
+        justifyContent={"center"}
+      >
+        {Children.toArray(
+          pages.map((page: SitePages) => (
+            <NavigationItem href={page.slug} label={page.name} />
+          ))
+        )}
+      </Box>
+      {/* empty div to push contetnt more on the left */}
+      <Box
+        flexGrow={0.1}
+        display={"flex"}
+        justifyContent={"space-between"}
+      ></Box>
     </Show>
   );
 }
---
+++ src/config/pages.ts
@@ -0,0 +1,23 @@
+export interface SitePages {
+  slug: string;
+  name: string;
+  type :string;
+}
+
+export const pages: SitePages[] = [
+  {
+    name: "Home",
+    slug: "/",
+    type: "link",
+  },
+  {
+    name: "About me",
+    slug: "/about",
+    type: 'link'
+  },
+  {
+    name: "Blog",
+    slug: "/blog",
+    type: 'link'
+  },
+];
